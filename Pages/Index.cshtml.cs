aabf3f7bacc34a86ee00dc74d0e2066eeab045eb
aliaslani <a77aslani@gmail.com>
2025-11-17 02:17:35 +0330

    Refactor project structure and update dependencies
    - Excluded MAUI platform-specific files from web build in mymauiApp.csproj.
    - Removed unused authorization middleware in Program.cs.
    - Updated MainActivity.cs to improve activity configuration and added OnCreate method.
    - Updated project dependencies to target .NET 10.0.100 and adjusted package references accordingly.
    - Cleaned up project.assets.json and project.nuget.cache for better dependency management.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Astrolo.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,10 +1,38 @@
-﻿using Android.App;
+﻿/*using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
 namespace mymauiApp;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme",  Label = "MyApp",
+    Icon = "@mipmap/icon",
+ MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
+  ConfigurationChanges = ConfigChanges.ScreenSize | 
+  Android.Content.PM.ConfigChanges.Orientation | ConfigChanges.UiMode | 
+  ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+}
+*/
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace mymauiApp;
+
+[Activity(
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize |
+                          Android.Content.PM.ConfigChanges.Orientation |
+                          Android.Content.PM.ConfigChanges.UiMode |
+                          Android.Content.PM.ConfigChanges.ScreenLayout |
+                          Android.Content.PM.ConfigChanges.SmallestScreenSize |
+                          Android.Content.PM.ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+    }
 }
